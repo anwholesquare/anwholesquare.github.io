00bd9fcc2c34e4616db49933fbe823fead4cc715
anwholesquare <anwholesquare@gmail.com>
2020-04-14 11:36:51 +0600

    INITIAL COMMIT

---
+++ checker.php
@@ -0,0 +1,15 @@
+<?php
+if(function_exists('mcrypt_encrypt')) {
+    echo "mcrypt is loaded!";
+} else {
+    echo "mcrypt isn't loaded!";
+}
+
+?>
+
+
+
+
+apt install php7.3 php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-mysql php7.3-zip php7.3-fpm php7.3-mbstring
+
+apt install libapache2-mod-php7.3 libapache2-mod-php
---
+++ target_gallery.php
@@ -0,0 +1,3 @@
+<?php 
+  echo file_get_contents("https://flickrembed.com/cms_embed.php?source=flickr&layout=responsive&input=www.flickr.com/photos/165646631@N07/&sort=0&by=user&theme=tiles_justified&scale=fill&speed=3000&limit=50&skin=alexis&autoplay=true");
+?>
